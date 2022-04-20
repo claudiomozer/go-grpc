2740c031f35caf61b66953520d407adc4e48cce4
Cláudio André Mozer <claudinhoandre@gmail.com>
2022-04-19 22:05:14 -0300

    feat: create go server and client with grpc

---
+++ greet/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	pb "greet/greetpb"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+var serverAddress string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, error := net.Listen("tcp", serverAddress)
+
+	if error != nil {
+		log.Fatalf("Failed to listen on %v\n", error)
+	}
+
+	log.Printf("Listening on %s\n", serverAddress)
+	s := grpc.NewServer()
+
+	if error = s.Serve(lis); error != nil {
+		log.Fatalf("Failed to serve on %v\n", error)
+	}
+}
